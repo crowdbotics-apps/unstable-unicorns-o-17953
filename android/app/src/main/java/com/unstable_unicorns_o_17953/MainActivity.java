25e785cff6b8cce4334b22b305df772857e6aa73
Crowdbotics <team@crowdbotics.com>
2020-06-08 22:06:57 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/unstable_unicorns_o_17953/MainActivity.java
@@ -0,0 +1,15 @@
+package com.unstable_unicorns_o_17953;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "unstable_unicorns_o_17953";
+    }
+}
